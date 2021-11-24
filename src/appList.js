1588ef75906cfb4aa9e6bf0d3f0741e347bd6e12
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2021-11-24 14:41:38 +0100

    fix bundling

--- src/appList.js
+++ src/appList.js
@@ -2,19 +2,19 @@ const appObjectList = {
   'Candy App': {
     icon: 'https://cdn4.iconfinder.com/data/icons/christmas-spirit-2/32/candy-256.png',
     bundle: async () =>
-      await import(/* webpackChunkName: candyapp */ '../candyapp'),
+      await import(/* webpackChunkName: 'candyapp' */ '../candyapp'),
   },
 
   'Ice Cream App': {
     icon: 'https://cdn2.iconfinder.com/data/icons/food-184/512/icecream_candy_food-256.png',
     bundle: async () =>
-      await import(/* webpackChunkName: icecreamapp */ '../icecreamapp'),
+      await import(/* webpackChunkName: 'icecreamapp' */ '../icecreamapp'),
   },
 
   'Lollipop App': {
     icon: 'https://cdn0.iconfinder.com/data/icons/kameleon-free-pack-rounded/110/Candy-256.png',
     bundle: async () =>
-      await import(/* webpackChunkName: lollipopapp */ '../lollipopapp'),
+      await import(/* webpackChunkName: 'lollipopapp' */ '../lollipopapp'),
   },
 };
 
@@ -26,7 +26,7 @@ const appArrayList = Object.keys(appObjectList).map(key => ({
 const defaultApp = {
   icon: 'https://cdn3.iconfinder.com/data/icons/animal-emoji/50/Sloth-256.png',
   bundle: async () =>
-    await import(/* webpackChunkName: defaultapp */ '../defaultapp'),
+    await import(/* webpackChunkName: 'defaultapp' */ '../defaultapp'),
 };
 
 const remoteApp = {
